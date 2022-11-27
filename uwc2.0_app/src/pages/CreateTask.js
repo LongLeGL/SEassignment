dacee45e2835951ffe6342508e72fddbecadce3a
Lưu Trịnh Thiên <luutrinhthien@MacBook-Pro-cua-Luu.local>
2022-11-27 21:59:30 +0700

    update create task

--- uwc2.0_app/src/pages/CreateTask.js
+++ uwc2.0_app/src/pages/CreateTask.js
@@ -5,7 +5,8 @@ import React from 'react';
 function CreateTask() {
 	return (
 		<div className='CreateTask'>
-				<div>Task Creation page</div>
+			<div>Task Creation page</div>
+			<div>New Task</div>
 		</div>
 	);
 }
