0acdee1ad236bab59c7aa551428f533e6f91e16b
Long Le <lhlong1542002@gmail.com>
2022-12-04 21:44:34 +0700

    widen Personnel table

--- uwc2.0_app/src/pages/PersonnelManage.js
+++ uwc2.0_app/src/pages/PersonnelManage.js
@@ -18,14 +18,14 @@ function PersonnelManage() {
     <div className="PersonnelManage">
       <div>
         <Grid container spacing={2}>
-          <Grid item xs={8}>
+          <Grid item xs={11}>
             <GridJS
               data={[
                 ["J0001", "Long Le Hoang", "longle69@uwc.com"],
                 ["J0002", "Nguyen Ngo Quang", "nguyenngo@uwc.com"],
-                ["J0003", "Khoi Le Quang", "smokeyle123@uwc.com"],
-                ["J0004", "Thien Luu Trinh", "skyluu@uwc.com"],
-                ["J0005", "Phuc Nguyen Nho Gia", "phucdeptrainguyen@uwc.com"],
+                ["C0001", "Khoi Le Quang", "smokeyle123@uwc.com"],
+                ["J0003", "Thien Luu Trinh", "skyluu@uwc.com"],
+                ["C0002", "Phuc Nguyen Nho Gia", "phucdeptrainguyen@uwc.com"],
               ]}
               columns={["ID", "Name", "Email"]}
               search={true}
