0e9d5f0475f069f2ddb15a432950f7ddc0626093
chitkenkhoi <117997639+chitkenkhoi@users.noreply.github.com>
2022-12-09 11:21:30 +0700

    Upload files for MCP and Vehicle pages

--- uwc2.0_app/src/pages/VehicleManage.js
+++ uwc2.0_app/src/pages/VehicleManage.js
@@ -1,22 +1,97 @@
 import "./global.css";
-import "./PersonnelManage.css";
+import "./VehicleManage.css";
 import React from "react";
 import { Grid as GridJS } from "gridjs-react";
-import Paper from "@mui/material/Paper";
+import{h} from "gridjs";
 import Grid from "@mui/material/Grid";
-import { styled } from "@mui/material/styles";
+import{useState,useRef,useEffect} from 'react';
 
-const Item = styled(Paper)(({ theme }) => ({
-  padding: theme.spacing(1),
-  textAlign: "center",
-}));
+
 
 function VehicleManage() {
+  const [id, setId] = useState("");
+  const data = [
+    {
+     
+      ID: "#T011",
+      type: "HUYNDAI",
+      price:"570 triệu đồng",
+    },
+    
+    {
+      ID: "#T017",
+      type: "HUYNDAI",
+      price:"570 triệu đồng",
+    },
+    {
+      ID: "#T013",
+      type: "HUYNDAI",
+      price:"570 triệu đồng",
+    },
+    {
+      ID: "#T019",
+      type: "HUYNDAI",
+      price:"570 triệu đồng",
+    },
+    {
+      ID: "#T021",
+      type: "HUYNDAI",
+      price:"570 triệu đồng",
+    },
+    {
+      ID: "#T035",
+      type: "HUYNDAI",
+      price:"570 triệu đồng",
+    },
+    {
+      ID: "#T077",
+      type: "HUYNDAI",
+      price:"570 triệu đồng",
+    },
+    {
+      ID: "#T002",
+      type: "HUYNDAI",
+      price:"570 triệu đồng",
+    },
+    {
+      ID: "#T012",
+      type: "HUYNDAI",
+      price:"570 triệu đồng",
+    },
+    {
+      ID: "#T030",
+      type: "HUYNDAI",
+      price:"570 triệu đồng",
+    },
+    {
+      ID: "#S02391",
+      type: "HONDA",
+      price:"100 triệu đồng",
+    },
+    {
+      ID: "#S01234",
+      type: "HONDA",
+      price:"100 triệu đồng",
+    },
+    {
+      ID: "#S12310",
+      type: "HONDA",
+      price:"100 triệu đồng",
+    },
+  ]
+  const foundtype = function () {
+		return data.find(obj => obj.ID === id)?.type;
+	}
+  const foundprice = function () {
+		return data.find(obj => obj.ID === id)?.price;
+	}
 	return (
 		<div className = 'VehicleManage'>
+			<Grid container spacing ={0}>
+				<Grid item xs={9}>
 		<div className='VehicleManage1'>
 			<div>
-				<h1 >
+				<h1 className="header">
 					Xe chở rác
 				</h1>
 			</div>
@@ -25,22 +100,32 @@ function VehicleManage() {
           <Grid item xs={11}>
             <GridJS
               data={[
-                ["1.", "#T011", "8.9","6.2 tấn","12.2L/100 KM"],
-				["2.", "#T017", "8.9","6.2 tấn","12.2L/100 KM"],
-				["3.", "#T013", "8.9","6.2 tấn","12.2L/100 KM"],
-				["4.", "#T019", "8.9","6.2 tấn","12.2L/100 KM"],
-				["5.", "#T021", "8.9","6.2 tấn","12.2L/100 KM"],
-				["6.", "#T035", "9.4","6.7 tấn","13.1L/100 KM"],
-				["7.", "#T077", "9.4","6.7 tấn","13.1L/100 KM"],
-				["8.", "#T002", "9.4","6.7 tấn","13.1L/100 KM"],
-				["9.", "#T012", "9.4","6.7 tấn","13.1L/100 KM"],
-				["10.", "#T030", "9.4","6.7 tấn","13.1L/100 KM"],
+                ["#T011", "8.9","6.2 tấn","12.2L/100 KM"],
+				["#T017", "8.9","6.2 tấn","12.2L/100 KM"],
+				["#T013", "8.9","6.2 tấn","12.2L/100 KM"],
+				["#T019", "8.9","6.2 tấn","12.2L/100 KM"],
+				["#T021", "8.9","6.2 tấn","12.2L/100 KM"],
+				["#T035", "9.4","6.7 tấn","13.1L/100 KM"],
+				["#T077", "9.4","6.7 tấn","13.1L/100 KM"],
+				["#T002", "9.4","6.7 tấn","13.1L/100 KM"],
+				["#T012", "9.4","6.7 tấn","13.1L/100 KM"],
+				["#T030", "9.4","6.7 tấn","13.1L/100 KM"],
               ]}
-              columns={["No.", "Mã số xe", "Trọng lượng", "Tải trọng tác tối đa", "Mức tiêu thụ nhiên liệu"]}
+              columns={[ "Mã số xe", "Trọng lượng", "Tải trọng rác tối đa", "Mức tiêu thụ nhiên liệu",
+              { 
+                name: 'Hành động',
+                formatter: (cell, row) => {
+                  return h('button', {
+                  className: 'py-2 mb-4 px-4 border rounded-md text-white bg-blue-600',
+                  onClick: () => {setId(row.cells[0].data) ; }
+                  }, 'Xem');
+                }
+              },]}
               search={true}
+			  height= '400px'
               pagination={{
                 enabled: true,
-                limit: 5,
+                limit: 10,
 				nextButton: true,
 				prevButton: true,
               }}
@@ -52,12 +137,17 @@ function VehicleManage() {
                 },
                 table: {
                   border: "3px solid #ccc",
+				  whitespace: "no wrap"
                 },
                 th: {
-                  "background-color": "rgba(0, 0, 0, 0.1)",
-                  color: "#000",
-                  "border-bottom": "3px solid #ccc",
-                  "text-align": "center",
+					"background-color": "rgba(0, 0, 0, 0.1)",
+					color: "#000",
+					"border-bottom": "3px solid #ccc",
+					"text-align": "center",
+					"width": "100%",
+					"padding": "0.5em 0.1em",
+					"font-weight": "600",
+					"font-size": "15px"
                 },
                 td: {
                   "text-align": "center",
@@ -69,9 +159,35 @@ function VehicleManage() {
         </Grid>
       </div>
 		</div>
+		</Grid>
+    <Grid item xs={3}>
+    <div className =  "Detailinformationphuongtien">
+			<div> {id}
+				</div>
+			<img src="" />
+			<div>
+				THÔNG TIN NHẬP KHO
+				<ul>
+				<li>Hãng: {foundtype()}</li>
+				<li>Ngày: 20/10/2016</li>
+				<li>Giá thành: {foundprice()}</li>
+			</ul>
+			</div>
+			
+			<div>
+				THÔNG TIN KỸ THUẬT
+				<ul>
+				<li>Chiều dài cơ sở: 4.28m</li>
+				<li>Dung tích: 200L</li>
+			</ul>
+			</div>
+			
+			</div>
+    </Grid>
+    </Grid>
 		<div className='VehicleManage2'>
 			<div>
-				<h1 id ="headeroftable">
+				<h1 className="header">
 					Xe đẩy
 				</h1>
 			</div>
@@ -80,15 +196,24 @@ function VehicleManage() {
           <Grid item xs={11}>
             <GridJS
               data={[
-                ["1.", "#T011", "8.9","6.2 tấn","12.2L/100 KM"],
-				["2.", "#T017", "8.9","6.2 tấn","12.2L/100 KM"],
-				["3.", "#T013", "8.9","6.2 tấn","12.2L/100 KM"],
+                ["#S02391", "82kg","200L"],
+				["#S01234", "82kg","200L"],
+				["#S12310", "82kg","200L"],
               ]}
-              columns={["No.", "Mã số xe", "Trọng lượng", "Dung tích"]}
+              columns={["Mã số xe", "Trọng lượng", "Dung tích", { 
+                name: 'Hành động',
+                formatter: (cell, row) => {
+                  return h('button', {
+                  className: 'py-2 mb-4 px-4 border rounded-md text-white bg-blue-600',
+                  onClick: () => {setId(row.cells[0].data) ; }
+                  }, 'Xem');
+                }
+              },]}
               search={true}
+			  height= '200px'
               pagination={{
                 enabled: true,
-                limit: 5,
+                limit: 10,
 				nextButton: true,
 				prevButton: true,
               }}
